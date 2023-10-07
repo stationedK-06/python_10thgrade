33548c10fc0c0f1d49ad7e6a6a254e2611273d16
jhkim <gan0809jh@gmail.com>
2023-10-06 23:57:30 -0500

    java project with robot

---
+++ joohan_kim_java project/src/MazeCleaner.java
@@ -0,0 +1,25 @@
+import kareltherobot.World;
+
+/*
+ * Name: Your Name
+ * Period: Your Period
+ * Program #2
+ * Any description you’d like to add or information you’d like to share with me.                   
+ */ 
+
+public class MazeCleaner 
+{
+
+	public static void main(String[] args) 
+	{
+		World.setVisible(); 
+		
+		// Change the path name to YOUR "s" number
+		// "Unit2ProgramWorlds\\s******\\world2.kwld"
+		World.readWorld("Unit2ProgramWorlds\\s000000\\world2.kwld"); 
+		
+		
+
+	}
+
+}
